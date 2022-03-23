9cc37686e920b2f3c3b8908ecb965536ed8d6d30
Artsem <Kyden184@gmail.com>
2022-03-23 16:20:56 +0300

    init

---
+++ pages/product_page.py
@@ -0,0 +1,37 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+
+class ProductPage(BasePage):
+
+
+    def add_to_basket(self):
+        cart_button = self.browser.find_element(*ProductPageLocators.add_cart)
+        cart_button.click()
+
+    def AlertAnswer(self):
+        self.solve_quiz_and_get_code()
+
+    def should_be_message_to_add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.saccess_message), "must be a message about add to cart"
+
+    def should_be_message_about_price_cart(self):
+        assert self.is_element_present(*ProductPageLocators.saccess_message), "must be a message about product price"
+
+    def product_name_valid(self):
+        book_name = self.browser.find_element(*ProductPageLocators.book_name).text
+        book_name_basket = self.browser.find_element(*ProductPageLocators.book_name_basket).text
+        assert book_name == book_name_basket, "Name is not same"
+
+    def product_price_valid(self):
+        book_price = self.browser.find_element(*ProductPageLocators.book_price).text
+        book_price_basket = self.browser.find_element(*ProductPageLocators.book_price_basket).text
+        assert book_price == book_price_basket, "Price is not same"
+
+
+
+
+
+
+
---
+++ test_product_page.py
@@ -0,0 +1,58 @@
+from .pages.product_page import ProductPage
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.AlertAnswer()
+
+def test_should_be_message_to_add_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.AlertAnswer()
+    page.should_be_message_to_add_to_cart()
+
+def test_should_be_message_about_price_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.AlertAnswer()
+    page.should_be_message_about_price_cart()
+
+def test_product_name_valid(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.AlertAnswer()
+    page.product_name_valid()
+
+def test_product_price_valid(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.AlertAnswer()
+    page.product_price_valid()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
